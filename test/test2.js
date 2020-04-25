8b1dfb1ac0015e1cb4995c1b06dffb9c52b536cd
Joe Pitts <joe@joelaptop.Home>
2020-04-25 14:02:30 +0100

    Remove file

--- test/test2.js
+++
@@ -1,5 +0,0 @@
-const reader = require("../index");
-
-reader.parseEpub("/home/joe/Downloads/Issac Asimov Foundation Series Complete [EDGE]/Isaac Asimov - Foundation.epub", (err, data) => {
-    console.log(data);
-})
